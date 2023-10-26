bc03c77ed2b1ffe7e7df55cebf8ebfd9f17d1664
David Huang <hxssg1124@gmail.com>
2023-10-26 21:07:11 +0800

    fix

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-PROJECT_DIR=${HOME}"/t5x/dir1/user_dir,/home/hxssgaa/flaxformer"
+PROJECT_DIR=${HOME}"/t5x/dir1/user_dir"
 T5X_DIR="/home/hxssgaa/t5x"  # directory where the t5x is cloned.
 TFDS_DATA_DIR="/home/hxssgaa/data"
 MODEL_DIR="gs://hxtpu_bucket/t5_2048"
