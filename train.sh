fa8d9fb95690614b5cbded73858a9760b111c56b
David Huang <hxssg1124@gmail.com>
2023-10-26 09:30:37 +0800

    fix

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 PROJECT_DIR=${HOME}"/t5x/dir1/user_dir"
 T5X_DIR="/home/hxssgaa/t5x"  # directory where the t5x is cloned.
 TFDS_DATA_DIR="/home/hxssgaa/data"
-MODEL_DIR="gs://hxtpu_bucket/t5_span"
+MODEL_DIR="gs://hxtpu_bucket/t5_2048"
 ACTIVATION_DTYPE=bfloat16
 export PYTHONPATH=${PROJECT_DIR}
 
@@ -11,6 +11,5 @@ python3 ${T5X_DIR}/t5x/train.py \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
   --gin.t5x.examples.scalable_t5.network.T5Config.dtype=\"${ACTIVATION_DTYPE}\" \
-  --gin.t5x.examples.scalable_t5.network.T5Config.remat_policy=\"minimal\" \
   --gin.utils.RestoreCheckpointConfig.dtype=\"${ACTIVATION_DTYPE}\" \
   --gin.DROPOUT_RATE=0.0
