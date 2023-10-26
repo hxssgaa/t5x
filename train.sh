adec19bee43688cf5dae9d8adc32e0664fa4769b
David Huang <hxssg1124@gmail.com>
2023-10-26 20:53:31 +0800

    fix

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="small_pretrain_dummy_wikipedia.gin" \
+  --gin_file="pretrain_dummy_wikipedia.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
   --gin.network.T5Config.dtype=\"${ACTIVATION_DTYPE}\" \
