56d31bff77c5a3daaa5bb7170b1fedd92b10b35f
David Huang <hxssg1124@gmail.com>
2023-10-30 14:53:23 +0800

    fix

--- finetune.sh
+++ finetune.sh
@@ -1,16 +1,30 @@
-PROJECT_DIR=${HOME}"/t5x/dir1/user_dir"
-T5X_DIR="/home/hxssgaa/t5x"  # directory where the t5x is cloned.
-TFDS_DATA_DIR="/home/hxssgaa/data"
-MODEL_DIR="/home/hxssgaa/models"  # "/home/hxssgaa/models"
-ACTIVATION_DTYPE=bfloat16
-export PYTHONPATH=${PROJECT_DIR}
+# PROJECT_DIR=${HOME}"/t5x/dir1/user_dir"
+# T5X_DIR="/home/hxssgaa/t5x"  # directory where the t5x is cloned.
+# TFDS_DATA_DIR="/home/hxssgaa/data"
+# MODEL_DIR="/home/hxssgaa/models"  # "/home/hxssgaa/models"
+# ACTIVATION_DTYPE=bfloat16
+# export PYTHONPATH=${PROJECT_DIR}
+
+# python3 ${T5X_DIR}/t5x/train.py \
+#   --gin_search_paths=${PROJECT_DIR} \
+#   --gin_file="finetune_large_squad1.gin" \
+#   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
+#   --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
+#   --gin.network.T5Config.dtype=\"${ACTIVATION_DTYPE}\" \
+#   --gin.utils.RestoreCheckpointConfig.dtype=\"${ACTIVATION_DTYPE}\" \
+#   --gin.INIT_CHECKPOINT=\"gs://t5-data/pretrained_models/t5x/t5_1_1_large/checkpoint_1000000\" \
+#   --gin.DROPOUT_RATE=0.1
+
+
+MODEL_DIR="/home/hxssgaa/models"
+
+# Data dir to save the processed dataset in "gs://data_dir" format.
+TFDS_DATA_DIR="/home/hxssgaa/tensorflow_datasets"
+T5X_DIR="/home/hxssgaa/t5x"  # directory where the T5X repo is cloned.
 
 python3 ${T5X_DIR}/t5x/train.py \
-  --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="finetune_large_squad1.gin" \
+  --gin_file="t5x/examples/t5/t5_1_1/examples/base_wmt19_ende_train.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
-  --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
-  --gin.network.T5Config.dtype=\"${ACTIVATION_DTYPE}\" \
-  --gin.utils.RestoreCheckpointConfig.dtype=\"${ACTIVATION_DTYPE}\" \
-  --gin.INIT_CHECKPOINT=\"gs://t5-data/pretrained_models/t5x/t5_1_1_large/checkpoint_1000000\" \
-  --gin.DROPOUT_RATE=0.1
+  --gin.MIXTURE_OR_TASK_NAME=\"wmt_t2t_ende_v003\" \
+  --gin.INIT_CHECKPOINT=\"gs://t5-data/pretrained_models/t5.1.1.base/model.ckpt-1000000\" \
+  --tfds_data_dir=${TFDS_DATA_DIR}
