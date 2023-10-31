5bc489ef5379f817696053ac0bb4fdb887909e69
David Huang <hxssg1124@gmail.com>
2023-10-31 20:51:46 +0800

    from scratch train sea LLM

---
+++ dir1/user_dir/sea_corpus_seqio.py
@@ -0,0 +1,32 @@
+import functools
+
+import seqio
+import t5.data
+from t5.data import preprocessors
+
+TaskRegistry = seqio.TaskRegistry
+
+DEFAULT_OUTPUT_FEATURES = {
+    "inputs": seqio.Feature(
+        vocabulary=t5.data.get_default_vocabulary(), add_eos=True,
+        required=False),
+    "targets": seqio.Feature(
+        vocabulary=t5.data.get_default_vocabulary(), add_eos=True)
+}
+
+# ================================ Wikipedia ===================================
+TaskRegistry.add(
+    "sea_corpus",
+    source=seqio.TextLineDataSource({'train': 'gs://hxtpu_bucket/sea_corpus/train_massive_filter.txt'}), #"wikipedia/20230601.en:1.0.0"),
+    preprocessors=[
+        functools.partial(
+            preprocessors.rekey, key_map={
+                "inputs": None,
+                "targets": "text"
+            }),
+        seqio.preprocessors.tokenize,
+        seqio.CacheDatasetPlaceholder(),
+        preprocessors.span_corruption,
+    ],
+    output_features=DEFAULT_OUTPUT_FEATURES,
+    metric_fns=[])
--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 PROJECT_DIR=${HOME}"/t5x/dir1/user_dir"
 T5X_DIR="/home/hxssgaa/t5x"  # directory where the t5x is cloned.
 TFDS_DATA_DIR="/home/hxssgaa/data"
-MODEL_DIR="gs://hxtpu_bucket/t5_xl_ada"
+MODEL_DIR="gs://hxtpu_bucket/t5_large_sea_ada"
 ACTIVATION_DTYPE=bfloat16
 export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="pretrain_xl_wikipedia.gin" \
+  --gin_file="pretrain_large_sea_corpus.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
   --gin.network.T5Config.dtype=\"${ACTIVATION_DTYPE}\" \
