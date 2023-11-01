bf198cfb7fe4353a5d45b13b8d65062be60142a6
David Huang <hxssg1124@gmail.com>
2023-11-01 10:26:15 +0800

    debug

--- dir1/user_dir/sea_corpus_seqio.py
+++ dir1/user_dir/sea_corpus_seqio.py
@@ -14,11 +14,16 @@ DEFAULT_OUTPUT_FEATURES = {
         vocabulary=t5.data.get_default_vocabulary(), add_eos=True)
 }
 
+def _process1(*x):
+    print('start _process 1')
+    import pdb; pdb.set_trace()
+
 # ================================ Wikipedia ===================================
 TaskRegistry.add(
     "sea_corpus",
     source=seqio.TextLineDataSource({'train': 'gs://hxtpu_bucket/sea_corpus/train_massive_filter.txt'}), #"wikipedia/20230601.en:1.0.0"),
     preprocessors=[
+        _process1,
         functools.partial(
             preprocessors.rekey, key_map={
                 "inputs": None,
