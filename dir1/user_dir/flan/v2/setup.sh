3480814c117f35c9d4ad19bd0f153e8c367f854a
David Huang <hxssg1124@gmail.com>
2023-11-02 15:15:51 +0800

    fix

--- dir1/user_dir/flan/v2/setup.sh
+++ dir1/user_dir/flan/v2/setup.sh
@@ -1,6 +1,3 @@
-pip install --upgrade pip
-pip install -r flan/v2/requirements.txt
-
 unzip flan/v2/niv2_few_shot_data/niv2_exemplars.jsonl-00000-of-00010.zip -d flan/v2/niv2_few_shot_data/
 unzip flan/v2/niv2_few_shot_data/niv2_exemplars.jsonl-00001-of-00010.zip -d flan/v2/niv2_few_shot_data/
 unzip flan/v2/niv2_few_shot_data/niv2_exemplars.jsonl-00002-of-00010.zip -d flan/v2/niv2_few_shot_data/
