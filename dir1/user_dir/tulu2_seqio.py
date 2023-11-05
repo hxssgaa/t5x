2829ad3931d07f611dc3fb925e56c50fb66a45cd
David Huang <hxssg1124@gmail.com>
2023-11-05 09:31:22 +0800

    fix

--- dir1/user_dir/tulu2_seqio.py
+++ dir1/user_dir/tulu2_seqio.py
@@ -17,6 +17,7 @@ DEFAULT_OUTPUT_FEATURES = {
 
 @seqio.map_over_dataset
 def _process1(x):
+    import pdb; pdb.set_trace()
     return json.loads(x)
 
 # ================================ Wikipedia ===================================
@@ -24,7 +25,7 @@ TaskRegistry.add(
     "tulu_v2",
     source=seqio.TextLineDataSource({'train': 'gs://hxtpu_bucket/inst_tuning/t5_tulu_v2_data.jsonl'}), #"wikipedia/20230601.en:1.0.0"),
     preprocessors=[
-        # _process1,
+        _process1,
         functools.partial(
             preprocessors.rekey, key_map={
                 "inputs": "inputs",
