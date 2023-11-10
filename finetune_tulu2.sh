0e428b024d9aaee3bd14f73f63b9638b25cf84a1
David Huang <hxssg1124@gmail.com>
2023-11-10 14:16:34 +0800

    fix

--- finetune_tulu2.sh
+++ finetune_tulu2.sh
@@ -7,7 +7,7 @@ export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="finetune_large_tulu2.gin" \
+  --gin_file="finetune_xl_tulu2.gin" \
   --gin.MODEL_DIR=\"${MODEL_DIR}\" \
   --gin.MIXTURE_OR_TASK_NAME=\"tulu_v2\" \
   --gin.ACTIVATION_DTYPE=\"${ACTIVATION_DTYPE}\" \
